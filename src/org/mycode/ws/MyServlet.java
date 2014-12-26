3f1a5a7faf9aed7adde3906cf8415d77240eaad0
zhangnanw <zhangnanw@126.com>
2014-12-26 11:30:51 +0800

    Initial commit

---
+++ src/org/mycode/ws/MyServlet.java
@@ -0,0 +1,16 @@
+package org.mycode.ws;
+
+import java.io.IOException;
+
+/**
+ * Created by zhangnan1 on 2014/10/20.
+ */
+public class MyServlet  {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: zhangnan1
+  Date: 2014/10/20
+  Time: 16:01
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>这是Index页面</title>
+  </head>
+  <body>
+测试页面
+  </body>
+</html>
